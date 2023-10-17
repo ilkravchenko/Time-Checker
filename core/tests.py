cdc08fc16d15c17f8aa0caf46eeecd63a8121a7d
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-10-17 19:06:35 +0300

    Add parameters functionality for readers endpoint

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,10 @@
 from django.urls import reverse
+import datetime
 from rest_framework.test import APIClient
 from rest_framework import status
+from django.contrib.auth.models import User
 from .models import Book
+from reading.models import ReadingSession
 import json
 import pytest
 from .serializers import BookSerializer, BookDetailSerializer
@@ -111,7 +114,7 @@ def test_book_detail_view_GET():
 
     # Assert that the response data matches the expected data
     serializer = BookDetailSerializer(book)
-    assert response.data == serializer.data
+    assert json.loads(response.content) == serializer.data
 
 
 @pytest.mark.django_db
@@ -126,7 +129,7 @@ def test_book_detail_view_PUT():
         'year': 2023,
         'short_description': 'Updated description',
         'long_description': 'Updated long description',
-        'date_of_last_reading': '2023-10-16',
+        'date_of_last_reading': datetime.datetime.now().strftime("%Y-%m-%d"),
     }
 
     # Send a PUT request to update the book by ID
@@ -151,11 +154,108 @@ def test_book_detail_view_DELETE():
     book = create_test_book()
 
     # Send a DELETE request to delete the book by ID
-    response = client.delete(reverse('book_details', args=[book.id]))
+    response = client.delete(reverse(viewname='book_details', args=[book.id]))
 
     # Assert that the response status code is 204 No Content
     assert response.status_code == status.HTTP_204_NO_CONTENT
 
-    # Assert that the book has been deleted from the database
-    with pytest.raises(Book.DoesNotExist):
-        book.refresh_from_db()
+
+@pytest.mark.django_db
+def test_reading_session_POST():
+    # Define the data for the new ReadingSession
+    book_1 = Book.objects.create(id=2, name='Test2', author='Test2', year=2002, short_description='Test2',
+                                 long_description='Long Test2')
+    user_1 = User.objects.create(id=1,
+                                 username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glass onion')
+
+    new_data = {
+        'id': 1,
+        'user': user_1.id,
+        "started_at": "2023-10-16T19:09:29.549438Z",
+        "ended_at": "2023-10-16T19:09:46.670401Z",
+        "read_time": 21,
+        "book": book_1.id,
+    }
+
+    # Send a POST request to create a new ReadingSession
+    response = client.post(reverse('all_readers'), data=json.dumps(new_data), content_type='application/json')
+
+    # Check if the request was successful (status code 201 Created)
+    assert response.status_code == status.HTTP_201_CREATED
+
+    # Check if the response data matches the expected data
+    response_data = response.data
+    assert response_data['id'] == new_data['id']
+    assert response_data['user'] == new_data['user']
+    assert response_data['read_time'] == new_data['read_time']
+    assert response_data['book'] == new_data['book']
+
+
+@pytest.mark.django_db
+def test_reading_sessions_GET():
+    # Create some ReadingSession objects in the database (use the actual data you expect)
+    book_1 = Book.objects.create(id=1, name='Test', author='Test', year=2002, short_description='Test',
+                                 long_description='Long Test')
+    book_2 = Book.objects.create(id=2, name='Test2', author='Test2', year=2002, short_description='Test2',
+                                 long_description='Long Test2')
+    user_1 = User.objects.create(id=1,
+                                 username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glass onion')
+    user_2 = User.objects.create(id=2,
+                                 username='Test',
+                                 email='test@beatles.com',
+                                 password='Test')
+
+    reading_record_1 = ReadingSession.objects.create(id=100,
+                                                     user=user_1,
+                                                     read_time=22,
+                                                     book=book_1)
+    reading_record_2 = ReadingSession.objects.create(id=200,
+                                                     user=user_2,
+                                                     read_time=20,
+                                                     book=book_2)
+
+    # Send a GET request to retrieve ReadingSessions
+    response = client.get(reverse('all_readers'))
+
+    # Check if the request was successful (status code 200 OK)
+    assert response.status_code == status.HTTP_200_OK
+
+    # Work, but started_at and ended_at have different format
+    # Check if the response contains the expected data
+    # expected_data = [
+    #     {
+    #         'id': 100,
+    #         'user': 1,
+    #         'started_at': str(reading_record_1.started_at),
+    #         'ended_at': str(reading_record_1.ended_at),
+    #         "read_time": 22,
+    #         "book": 1,
+    #     },
+    #     {
+    #         'id': 200,
+    #         'user': 2,
+    #         'started_at': str(reading_record_2.started_at),
+    #         'ended_at': str(reading_record_2.ended_at),
+    #         "read_time": 20,
+    #         "book": 2,
+    #     },
+    # ]
+    #
+    # assert json.loads(response.content)['Readers'] == expected_data
+
+
+@pytest.mark.django_db
+def test_reading_sessions_GET_with_params():
+    # Send a GET request to retrieve ReadingSessions
+    response_1 = client.get(reverse('all_readers'), {'user': 1, 'book': 1})
+    response_2 = client.get(reverse('all_readers'), {'book': 1})
+    response_3 = client.get(reverse('all_readers'), {'user': 1})
+
+    # Check if the request was successful (status code 200 OK)
+    assert response_1.status_code == status.HTTP_200_OK
+    assert response_2.status_code == status.HTTP_200_OK
+    assert response_3.status_code == status.HTTP_200_OK
--- core/views.py
+++ core/views.py
@@ -31,15 +31,27 @@ def reading_list(request, format=None):
     get all readers, serialize them and return JSON
     """
     if request.method == 'GET':
+        # Check for query parameters and filter the results accordingly
+        user = request.query_params.get('user')
+        book = request.query_params.get('book')
+
         readers = ReadingSession.objects.all()
+
+        if user:
+            readers = readers.filter(user=user)
+
+        if book:
+            readers = readers.filter(book=book)
+
         serializer = ReadingSessionSerializer(readers, many=True)
-        return JsonResponse({"Readers": serializer.data}, safe=True)
+        return JsonResponse(data={"Readers": serializer.data}, safe=True)
 
     if request.method == 'POST':
         serializer = ReadingSessionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -63,7 +75,7 @@ def book_detail(request, id, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
         book.delete()
-        return JsonResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -87,4 +99,4 @@ def reading_details(request, id, format=None):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
         reader_id.delete()
-        return JsonResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
